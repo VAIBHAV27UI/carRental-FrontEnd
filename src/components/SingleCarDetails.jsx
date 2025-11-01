7fc97ee0121290c7f1ffebe9506c2b79df1b7be3
Vaibhav Jagtap <vaibhavsocial7@gmail.com>
2025-11-01 21:01:56 +0530

    responsive

--- src/components/SingleCarDetails.jsx
+++ src/components/SingleCarDetails.jsx
@@ -38,7 +38,7 @@ const SingleCarDetails = (props) => {
 
   return (
     <div className="max-w-7xl mx-auto">
-      <div className="grid md:grid-cols-4 gap-5 w-full">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-5 w-full">
         <div className="md:col-span-3">
           <div>
             <h1 className="text-4xl font-bold">{title}</h1>
